8c8113bf4096e8f0d5d91b81f05d79b86674eea3
Christian Cuestas <31107409+ChrisCuestas@users.noreply.github.com>
2018-03-07 17:46:08 -0500

    Update LinkedList.java

--- EstructurasDatos/src/ed2018/LinkedList.java
+++ EstructurasDatos/src/ed2018/LinkedList.java
@@ -1,4 +1,82 @@
-package ed2018;
+package data;
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+/**
+ * This is an generic and abstract list, that implements attributes and methods 
+ * that are invariable regarded to a particular implementation. Also, it has the basic
+ * abstract methods that each implementation must implement.
+ * 
+ * @author Christian Camilo Cuestas Ibáñez
+ *
+ * @param <E> the type of object to store in the list.
+ */
+
+abstract class List<E> {
+
+	protected int size;
+	
+	public List() {
+		this.size=0;
+	}
+	
+	public int getSize() {
+		return this.size;
+	}
+	
+	protected void setSize(int size) {
+		this.size=size;
+	}
+	
+	public void incrementSize(int increment) {
+		this.setSize(this.getSize()+increment);
+	}
+	
+	public void decrementSize(int decrement) {
+		this.setSize(this.getSize()-decrement);
+	}
+	
+	public boolean isEmpty() {
+		return this.size==0;
+	}
+	
+	public abstract void makeEmpty();
+	
+	public abstract boolean insert(int index, E element);
+	
+	public void insertFirst(E element) {
+		this.insert(0, element);
+	};
+	
+	public void insert(E element) {
+		this.insert(this.getSize(), element);
+	}
+	
+	public abstract E get(int index);
+	
+	public E getFirst() {
+		return this.get(0);
+	}
+	
+	public E getLast() {
+		return this.get(this.getSize()-1);
+	}
+	
+	public abstract E remove(int index);
+	
+	public E removeFirst() {
+		return this.remove(0);
+	}
+	
+	public E removeLast() {
+		return this.remove(this.getSize()-1);
+	}
+	
+	public abstract E replace(int index, E newElement);
+	
+	public abstract int firstIndexOf(E element);
+}
 
 /**
  * This  is an object that wraps a simple-linked list based on nodes.
@@ -24,7 +102,7 @@ public class LinkedList<E> extends List<E> {
 		}
 		@Override
 		public String toString(){
-			return "Node: ["+ element.toString() +"]" + "\n";
+			return "["+ element.toString() +"]" + "\n";
 		}
 	}
 	
@@ -149,4 +227,21 @@ public class LinkedList<E> extends List<E> {
 		}	
 		return -1;
 	}	
+	
+	public void printList() {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		Node<E> temp = this.head;
+		
+		try {
+			bw.write("Grades:\n");
+			while (temp!=null){
+				bw.write(temp.toString());
+				temp = temp.nextNode;
+			}
+			bw.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
+
