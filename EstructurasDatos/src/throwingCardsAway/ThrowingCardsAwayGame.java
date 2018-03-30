45dd8aab1d2fb560b540a2bdea4efa5e4402f4d9
ChrisCuestas <cccuestasi@unal.edu.co>
2018-03-30 17:40:59 -0500

    Added a new problem solution

---
+++ EstructurasDatos/src/throwingCardsAway/ThrowingCardsAwayGame.java
@@ -0,0 +1,67 @@
+package throwingCardsAway;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import ed2018.LinkedList;
+import ed2018.Queue;
+
+public class ThrowingCardsAwayGame {
+
+	Queue<Integer> deck;
+	Queue<Integer> discardedCards;
+	
+	public ThrowingCardsAwayGame() {
+		this.deck = new Queue<Integer>();
+		this.discardedCards = new Queue<Integer>();
+	}
+
+	public void play(int n) {
+		for(int i=1; i<n+1; i++) {
+			this.deck.insert(i);
+		}
+		while(deck.getSize()>1) {
+			this.discardedCards.enqueue(this.deck.dequeue());
+			this.deck.insert(this.deck.dequeue());
+		}
+	}
+	
+	public String getDiscardedCards(){
+		String discardedCards = "Discarded cards: ";
+		while(!this.discardedCards.isEmpty()) {
+			discardedCards += this.discardedCards.dequeue();
+		}
+		return discardedCards;
+	}
+	
+	public String getRemainingCard() {
+		String remainingCard = "Remaining card: ";
+		remainingCard += this.deck.dequeue();
+		return remainingCard;
+	}
+	
+	public static void main(String[] args) {
+		ThrowingCardsAwayGame th;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		try {
+			int n = Integer.parseInt(br.readLine());
+			while(n!=0) {
+				th= new ThrowingCardsAwayGame();
+				th.play(n);
+				bw.write(th.getDiscardedCards() + "\n");
+				bw.write(th.getRemainingCard() + "\n");
+				bw.flush();
+				n = Integer.parseInt(br.readLine());
+			}
+			br.close();
+			bw.close();
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+}
