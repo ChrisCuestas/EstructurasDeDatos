adc5e74667b5c85aaf0995ce05ee5549811cdf89
Christian Cuestas <31107409+ChrisCuestas@users.noreply.github.com>
2018-04-16 17:28:54 -0500

    Boosted

--- EstructurasDatos/src/ed2018/Node.java
+++ EstructurasDatos/src/ed2018/Node.java
@@ -4,18 +4,20 @@ public class Node<T> {
 	T element;
 	int value;
 	Node<T> nextNode;
-	public Node(T element, Node<T> nextNode, int value) {
+	Node<T> lastNode;
+	public Node(T element, Node<T> lastNode,  Node<T> nextNode, int value) {
 		super();
 		this.element = element;
 		this.nextNode = nextNode;
+		this.lastNode = lastNode;
 		this.value = value;
-	}
+}
 	@Override
 	public String toString(){
 		return "["+ element.toString() +"]" + "\n";
 	}
 	
 	public Node<T> clone(){
-		return new Node<T>(this.element, this.nextNode.clone(),this.value);
+		return new Node<T>(this.element, this.lastNode.clone(), this.nextNode.clone(),this.value);
 	}
 }
