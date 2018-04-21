6e0c35480b344786607b8af860785bef13050b91
ChrisCuestas <cccuestasi@unal.edu.co>
2018-04-21 15:59:47 -0500

    Implemented some methods.

--- EstructurasDatos/src/ed2018/BinaryTree.java
+++ EstructurasDatos/src/ed2018/BinaryTree.java
@@ -2,41 +2,139 @@ package ed2018;
 
 public class BinaryTree<E> {
 
-	TreeNode<E> root;
+	private E root;
+	private int value;
+	private BinaryTree<E> left;
+	private BinaryTree<E> right;
 	
 	public BinaryTree() {
-		// TODO Auto-generated constructor stub
+		this.value = -1;
+		this.left = null;
+		this.right = null;
 	}
 	
-
+	public BinaryTree(E element, int value) {
+		this.value = value;
+		this.root = element;
+		this.left = null;
+		this.right = null;
+	}
+	
+	public BinaryTree(E element, int value, BinaryTree<E> left, BinaryTree<E> right) {
+		this.value = value;
+		this.root = element;
+		this.left = left;
+		this.right = left;
+	}
+	
 	public E getRoot() {
+		return this.root;
+	}
+	
+	public void setRoot(E root) {
+		this.root = root;
+	}
+	
+	public void setRoot(BinaryTree<E> node) {
+		this.root = node.root;
+		this.value = node.value;
+		this.left = node.left;
+		this.right = node.right;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public BinaryTree<E> getLeft() {
+		return left;
+	}
+
+	public void setLeft(BinaryTree<E> left) {
+		this.left = left;
+	}
+
+	public BinaryTree<E> getRight() {
+		return right;
+	}
+
+	public void setRight(BinaryTree<E> right) {
+		this.right = right;
+	}
+
+	public boolean insert(E element, int value) {
+		return insert(this, element, value);
+	}
+
+	private boolean insert(BinaryTree<E> node, E element, int value) {
+		if(value==node.value) return false;
+		if(value<node.value) {
+			if(node.left==null) {
+				node.left= new BinaryTree<E>(element, value);
+				return true;
+			}
+			else return insert(node.left, element, value);
+		}else {
+			if(node.right==null) {
+				node.right= new BinaryTree<E>(element, value);
+				return true;
+			}
+			else return insert(node.right, element, value);
+		}
+	}
+
+	public BinaryTree<E> removeLeftNode(BinaryTree<E> node) {
+		BinaryTree<E> leftSon = node.left;
+		if(leftSon==null) return null;
+ 		if(leftSon.left==null&&leftSon.right==null) 
+			node.left= null;
+		else {
+			if(leftSon.left==null)
+				node.left = leftSon.right;
+			else if(leftSon.right==null)
+				node.left = leftSon.left;
+			else {
+				BinaryTree<E> next = next(leftSon);
+				//TODO: Complete.
+			}
+		}
+ 		leftSon.right=leftSon.right=null;
+ 		return leftSon;
+	}
+	
+	public BinaryTree<E> next(BinaryTree<E> node) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public void insert(E data) {
+	public BinaryTree<E> getNode(int value) {
 		// TODO Auto-generated method stub
-		
+		return null;
 	}
 
-	public void remove(E data) {
-		// TODO Auto-generated method stub
-		
+	public BinaryTree<E> removeLeftSubtree(BinaryTree<E> node) {
+		if (root == null)
+			throw new IllegalArgumentException("This tree is empty");
+		BinaryTree<E> removedNode= node.left;
+		node.left= null;
+		return removedNode;
 	}
 
+	public BinaryTree<E> removeRightSubtree(BinaryTree<E> node) {
+		if (root == null)
+			throw new IllegalArgumentException("This tree is empty");
+		BinaryTree<E> removedNode= node.right;
+		node.right= null;
+		return removedNode;
+	}
+	
 	public void makeTree(E root, E left, E right) {
 		// TODO Auto-generated method stub
 		
-	}
-
-	public BinaryTree<E> removeLeft() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public BinaryTree<E> removeRight() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 	public E findMin() {
