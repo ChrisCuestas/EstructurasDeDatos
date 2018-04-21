a920a8be6815f923285a49d39e741792452ae094
ChrisCuestas <cccuestasi@unal.edu.co>
2018-04-21 14:28:33 -0500

    New data structure

--- EstructurasDatos/src/ed2018/BinaryTree.java
+++ EstructurasDatos/src/ed2018/BinaryTree.java
@@ -1,21 +1,105 @@
 package ed2018;
 
-public interface BinaryTree<T> {
+public class BinaryTree<E> implements BinTree<E>{
+
+	E root;
+	BinaryTree<E> left;
+	BinaryTree<E> right;
 	
-	public T getRoot();
-	public void insert(T data);
-	public void remove(T data);
-	public void makeTree(T root, T left, T right);
-	public BinaryTree<T> removeLeft();
-	public BinaryTree<T> removeRight();
-	public T findMin();
-	public T findMax();
-	public Object[] preOrder();
-	public Object[] inOrder();
-	public Object[] postOrder();
-	public Object[] levelOrder()
-	public boolean contains(T data);
-	public void makeEmpty();
-	public boolean isEmpty();
-	
+	public BinaryTree(E element) {
+		this.root = element;
+		this.left = null;
+		this.right = null;
+	}
+
+	@Override
+	public E getRoot() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insert(E data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove(E data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void makeTree(E root, E left, E right) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public BinTree<E> removeLeft() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BinTree<E> removeRight() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E findMin() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E findMax() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object[] preOrder() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object[] inOrder() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object[] postOrder() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object[] levelOrder() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean contains(E data) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void makeEmpty() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
