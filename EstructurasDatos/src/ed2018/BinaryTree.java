a7afd2a5ce93ddba9fe1fe4c0360a20b27458dd1
Estudiante <Estudiante@10.203.147.125>
2018-05-02 17:49:09 -0500

    Updated

--- EstructurasDatos/src/ed2018/BinaryTree.java
+++ EstructurasDatos/src/ed2018/BinaryTree.java
@@ -65,6 +65,10 @@ public class BinaryTree<E> {
 	public void setRight(BinaryTree<E> right) {
 		this.right = right;
 	}
+	
+	public String toString() {
+		return "Value: " + this.value + "\n";
+	}
 
 	public boolean insert(E element, int value) {
 		return insert(this, element, value);
@@ -72,6 +76,11 @@ public class BinaryTree<E> {
 
 	private boolean insert(BinaryTree<E> node, E element, int value) {
 		if(value==node.value) return false;
+		if(node.value==-1) {
+			node.root=element;
+			node.value=value;
+			return true;
+		}
 		if(value<node.value) {
 			if(node.left==null) {
 				node.left= new BinaryTree<E>(element, value);
@@ -88,25 +97,27 @@ public class BinaryTree<E> {
 	}
 
 	public BinaryTree<E> removeLeftNode(BinaryTree<E> node) {
-		BinaryTree<E> leftSon = node.left;
-		if(leftSon==null) return null;
- 		if(leftSon.left==null&&leftSon.right==null) 
-			node.left= null;
-		else {
-			if(leftSon.left==null)
-				node.left = leftSon.right;
-			else if(leftSon.right==null)
-				node.left = leftSon.left;
-			else {
-				BinaryTree<E> next = next(leftSon);
-				//TODO: Complete.
+		BinaryTree<E> leftSon = node.left;						//Get the left son of the given node
+		if(leftSon==null) return null;							//If there is not left node do nothing
+ 		if(leftSon.left==null&&leftSon.right==null) 			//but if the left son is a leaf
+			node.left= null;										//just set the left son as null
+		else {													//but other wise, we must handle the sons of leftSon
+			if(leftSon.left==null)									//while there is no left son of the leftSon 
+				node.left = leftSon.right;								//just set the right son of leftSon as the left son of the given node
+			else if(leftSon.right==null)							//but the right son of leftSon is null
+				node.left = leftSon.left;								//set the left son of the leftSon as the left son of the given node
+			else {													//other wise, we must handle both sons of leftSon
+				BinaryTree<E> next = removeNext(leftSon);						//get and remove the next node of the leftSon, that is a leaf
+				next.left = leftSon.left;										//set the sons of leftSon as the sons of next
+				next.right = leftSon.right;
+				node.left=next;													//set next as the new left son of the given node
 			}
 		}
  		leftSon.right=leftSon.right=null;
  		return leftSon;
 	}
 	
-	public BinaryTree<E> next(BinaryTree<E> node) {
+	public BinaryTree<E> removeNext(BinaryTree<E> node) {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -147,19 +158,28 @@ public class BinaryTree<E> {
 		return null;
 	}
 
-	public Object[] preOrder() {
-		// TODO Auto-generated method stub
-		return null;
+	public void preOrder(BinaryTree<E> node) {
+		if(node != null) {
+			System.out.println(node.toString());
+			preOrder(node.left);
+			preOrder(node.right);
+		}
 	}
 
-	public Object[] inOrder() {
-		// TODO Auto-generated method stub
-		return null;
+	public void inOrder(BinaryTree<E> node) {
+		if(node != null) {
+			inOrder(node.left);
+			System.out.println(node.toString());
+			inOrder(node.right);
+		}
 	}
 
-	public Object[] postOrder() {
-		// TODO Auto-generated method stub
-		return null;
+	public void postOrder(BinaryTree<E> node) {
+		if(node != null) {
+			postOrder(node.left);
+			postOrder(node.right);
+			System.out.println(node.toString());
+		}
 	}
 
 	public Object[] levelOrder() {
@@ -182,5 +202,26 @@ public class BinaryTree<E> {
 		return false;
 	}
 
+	public static void main(String[] args) {
+		BinaryTree<Integer> arbol = new BinaryTree<Integer>();
+		arbol.insert(5,82);
+		arbol.insert(42,85);
+		arbol.insert(20,55);
+		arbol.insert(2,38);
+		arbol.insert(1,70);
+		arbol.insert(4,2);
+		arbol.insert(4,81);
+		System.out.println("InOrder:");
+		arbol.inOrder(arbol);
+		System.out.println("PreOrder:");
+		arbol.preOrder(arbol);
+		System.out.println("PostOrder:");
+		arbol.postOrder(arbol);
+		arbol.remove(81);
+		
+	}
 
+	private void remove(int value) {
+		this.getValue(this.get);
+	}
 }
