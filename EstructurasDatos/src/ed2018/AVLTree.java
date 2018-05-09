f8c8917fb382cd22163c0ce3ac7317b646a2f54d
Estudiante <cccuestasi@unal.edu.co>
2018-05-09 17:54:25 -0500

    Update

---
+++ EstructurasDatos/src/ed2018/AVLTree.java
@@ -0,0 +1,24 @@
+package ed2018;
+
+public class AVLTree<E> extends BinaryTree<E> {
+
+	public AVLTree() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public AVLTree(E element, int value) {
+		super(element, value);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AVLTree(E element, int value, TreeNode<E> left, TreeNode<E> right) {
+		super(element, value, left, right);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ EstructurasDatos/src/gobletOnFire/Student.java
@@ -0,0 +1,32 @@
+package gobletOnFire;
+
+public class Student {
+
+	int school;
+	int rollNum;
+	
+	public Student(int school, int rollNumber) {
+		this.school = school;
+		this.rollNum = rollNumber;
+	}
+
+	public int getSchool() {
+		return school;
+	}
+
+	public void setSchool(int school) {
+		this.school = school;
+	}
+
+	public int getRollNum() {
+		return rollNum;
+	}
+
+	public void setRollNum(int rollNum) {
+		this.rollNum = rollNum;
+	}
+
+	public String toString() {
+		return this.school + " " + this.rollNum;
+	}
+}
