0cf6ff12046db81f5b0bc5b49982b4f8a0f3b6c2
ChrisCuestas <cccuestasi@unal.edu.co>
2018-05-15 18:00:55 -0500

    Problem still doesnt run

--- EstructurasDatos/src/ed2018/orderOfPhoenix/Army.java
+++ EstructurasDatos/src/ed2018/orderOfPhoenix/Army.java
@@ -1,14 +1,50 @@
 package ed2018.orderOfPhoenix;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import ed2018.Queue;
+import ed2018.Stack;
+
 public class Army {
 
-	public Army() {
-		// TODO Auto-generated constructor stub
+	int size;
+	Object[] rows;
+	public Army(int size) {
+		this.size = size;
+		this.rows = new Object[size];
+		for(int i=0; i<size; i++) rows[i]=new Stack<Integer>();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void insert(int row, int height) {
+		if(row>=this.size)return;
+		((Stack<Integer>) this.rows[row]).push(height);
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+		try {
+			int n = Integer.parseInt(br.readLine());
+			Army a = new Army(n);
+			for(int i=0; i<n; i++) {
+				String[] line = br.readLine().split(" ");
+				for(int j=1; j<line.length;j++)
+					a.insert(Integer.parseInt(line[0]), Integer.parseInt(line[j]));
+			}
+			
+			
+			bw.flush();
+			bw.close();
+			br.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
