e1e9fecca6d459bac39c29914fb556c0cee5d2c2
ChrisCuestas <cccuestasi@unal.edu.co>
2018-05-15 17:46:59 -0500

    New problem solution

---
+++ EstructurasDatos/src/ed2018/orderOfPhoenix/Army.java
@@ -0,0 +1,14 @@
+package ed2018.orderOfPhoenix;
+
+public class Army {
+
+	public Army() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
