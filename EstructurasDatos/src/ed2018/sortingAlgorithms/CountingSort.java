fd33b2ee65973d44384827a77b793fe34c517255
ChrisCuestas <cccuestasi@unal.edu.co>
2018-06-04 16:51:15 -0500

    Added new sorting algorithm, CountingSort

--- EstructurasDatos/src/ed2018/sortingAlgorithms/CountingSort.java
+++ EstructurasDatos/src/ed2018/sortingAlgorithms/CountingSort.java
@@ -1,11 +1,25 @@
 package ed2018.sortingAlgorithms;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class CountingSort {
 
 	private int[] array;
 	
 	public CountingSort(int[] array) {
 		this.array = array;
+	}
+	
+	public String printSortedArray() {
+		String line = "";
+		for(int i: this.array) {
+			line += i+ ", ";
+		}
+		return line;
 	}
 	
 	public void sort() {
@@ -30,8 +44,30 @@ public class CountingSort {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		CountingSort cs;
+		
+		try {
+			
+			String[] newString = br.readLine().split(","); 
+			br.close();
+			
+			int newSize= newString.length;
+			int[] newArray = new int[newSize];
+			for(int i=0; i<newSize; i++) {
+				newArray[i]=Integer.parseInt(newString[i]);
+			}
+			
+			cs = new CountingSort(newArray);
+			cs.sort();
+			
+			bw.write(cs.printSortedArray()+"\n");
+			bw.flush();
+			bw.close();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
